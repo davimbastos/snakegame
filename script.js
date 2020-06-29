a759b2d6e7457c2a1d75e22ce489f0a2ac7ec87e
davimbastos <davimbastos@gmail.com>
2020-06-29 17:08:36 -0300

    Difinição das teclas direcionais e reposicionamento ao alcançar as extremidades do canvas

--- script.js
+++ script.js
@@ -20,7 +20,31 @@ function createSnake(){
     }
 }
 
+document.addEventListener('keydown', update);
+
+function update(event) {
+    if(event.keyCode == 37 && direction != "right") 
+        direction = "left";
+    if(event.keyCode == 38 && direction != "up")
+        direction = "down";
+    if(event.keyCode == 39 && direction != "left")
+        direction = "right";
+    if(event.keyCode == 40 && direction != "down")
+        direction = "up";
+}
+
 function gameStart(){
+
+    if(snake[0].x > 15 * box && direction == "right")
+        snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left")
+        snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "up")
+        snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "down")
+        snake[0].y = 16 * box;
+
+
     criarBG();
     createSnake();
 
@@ -43,3 +67,4 @@ function gameStart(){
 }
 
 let jogo = setInterval(gameStart, 100);
+
